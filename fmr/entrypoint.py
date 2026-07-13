9b217ad922551598af3445068fcd4f51f6d63333
thedanieltan <danieltanyl91@gmail.com>
2026-07-14 03:35:49 +0800

    Compose financial data CLI commands

--- fmr/entrypoint.py
+++ fmr/entrypoint.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 import sys
 
 from fmr.dispatch import main as dispatch_main
+from fmr.financial_data_dispatch import (
+    FINANCIAL_DATA_COMMANDS,
+    run_financial_data_command,
+)
 from fmr.input_dispatch import INPUT_COMMANDS, run_input_command
 
 
 def main(argv: list[str] | None = None) -> int:
     arguments = list(sys.argv[1:] if argv is None else argv)
+    if arguments and arguments[0] in FINANCIAL_DATA_COMMANDS:
+        return run_financial_data_command(arguments)
     if arguments and arguments[0] in INPUT_COMMANDS:
         return run_input_command(arguments)
     return dispatch_main(arguments)
