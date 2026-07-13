2b6261e530ad592990ffddd95fe7ddae71f87d86
thedanieltan <danieltanyl91@gmail.com>
2026-07-14 03:32:51 +0800

    Expose provider-neutral financial data API

---
+++ fmr/financial_data/__init__.py
@@ -0,0 +1,36 @@
+"""Provider-neutral financial-data intake and workbook input binding."""
+
+from fmr.financial_data.binding import (
+    build_binding_profile,
+    compile_input_set_from_binding_plan,
+    plan_financial_input_bindings,
+    validate_binding_plan,
+    validate_binding_profile,
+)
+from fmr.financial_data.common import CONCEPTS, concept_registry_payload
+from fmr.financial_data.mapping import (
+    build_mapping_profile,
+    map_financial_data,
+    validate_mapping_profile,
+    validate_mapping_result,
+)
+from fmr.financial_data.package import (
+    import_statement_csv,
+    validate_financial_data_package,
+)
+
+__all__ = [
+    "CONCEPTS",
+    "build_binding_profile",
+    "build_mapping_profile",
+    "compile_input_set_from_binding_plan",
+    "concept_registry_payload",
+    "import_statement_csv",
+    "map_financial_data",
+    "plan_financial_input_bindings",
+    "validate_binding_plan",
+    "validate_binding_profile",
+    "validate_financial_data_package",
+    "validate_mapping_profile",
+    "validate_mapping_result",
+]
