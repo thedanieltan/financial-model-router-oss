144135f71dbc8c2d2529ca6fb02b04e0c4621583
thedanieltan <danieltanyl91@gmail.com>
2026-07-13 20:17:42 +0800

    Expose coordinate planner and bump package version

--- fmr/__init__.py
+++ fmr/__init__.py
@@ -5,11 +5,14 @@ from fmr.router import route_request
 from fmr.workbook import (
     analyse_workbook_map,
     compile_workbook_patch,
+    coordinate_rule_registry_payload,
     derive_workbook_evidence,
     inspect_workbook,
     inspect_workbook_bytes,
     operation_spec_registry_payload,
+    plan_workbook_coordinates,
     resolve_workbook_patch_targets,
+    validate_workbook_coordinate_plan_payload,
     validate_workbook_patch_payload,
     validate_workbook_patch_receipt_payload,
     validate_workbook_target_resolution_payload,
@@ -19,14 +22,17 @@ __all__ = [
     "analyse_workbook_map",
     "build_plan",
     "compile_workbook_patch",
+    "coordinate_rule_registry_payload",
     "derive_workbook_evidence",
     "inspect_workbook",
     "inspect_workbook_bytes",
     "operation_spec_registry_payload",
+    "plan_workbook_coordinates",
     "resolve_workbook_patch_targets",
     "route_request",
+    "validate_workbook_coordinate_plan_payload",
     "validate_workbook_patch_payload",
     "validate_workbook_patch_receipt_payload",
     "validate_workbook_target_resolution_payload",
 ]
-__version__ = "0.3.1"
+__version__ = "0.3.2"
