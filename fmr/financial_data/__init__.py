906cefeb2f96bd0e3b027ceda5545219f971fd4d
thedanieltan <danieltanyl91@gmail.com>
2026-07-17 19:20:11 +0800

    WP-FMR-PUX-30: expose canonical workflow source compiler

--- fmr/financial_data/__init__.py
+++ fmr/financial_data/__init__.py
@@ -7,6 +7,14 @@ from fmr.financial_data.binding import (
     validate_binding_plan,
     validate_binding_profile,
 )
+from fmr.financial_data.canonical import (
+    STATEMENT_CSV_COLUMNS,
+    WorkflowSourceStore,
+    compile_canonical_financial_data,
+    create_statement_csv_workflow_source,
+    derive_available_data,
+    statement_csv_template,
+)
 from fmr.financial_data.common import CONCEPTS, concept_registry_payload
 from fmr.financial_data.mapping import (
     build_mapping_profile,
@@ -21,13 +29,19 @@ from fmr.financial_data.package import (
 
 __all__ = [
     "CONCEPTS",
+    "STATEMENT_CSV_COLUMNS",
+    "WorkflowSourceStore",
     "build_binding_profile",
     "build_mapping_profile",
+    "compile_canonical_financial_data",
     "compile_input_set_from_binding_plan",
     "concept_registry_payload",
+    "create_statement_csv_workflow_source",
+    "derive_available_data",
     "import_statement_csv",
     "map_financial_data",
     "plan_financial_input_bindings",
+    "statement_csv_template",
     "validate_binding_plan",
     "validate_binding_profile",
     "validate_financial_data_package",
