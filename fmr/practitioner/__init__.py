28b774a3282ed5d0744f616026727368be48ebc4
thedanieltan <danieltanyl91@gmail.com>
2026-07-18 03:14:59 +0800

    Add practitioner workflow package

---
+++ fmr/practitioner/__init__.py
@@ -0,0 +1,6 @@
+"""Practitioner-facing workflows.
+
+These modules are deliberately separate from the provider-routing internals.
+They expose workflow-first surfaces that finance users can run without
+understanding manifests, handoffs or JSON contracts.
+"""
